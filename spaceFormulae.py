487c7aef812dbcac95966ad0726c4d4b4ae06f94
VarIsid <49831030+VarIsid@users.noreply.github.com>
2019-10-06 13:25:27 +0200

    SPACE: added excel + started with cariables for formulae

---
+++ spaceFormulae.py
@@ -0,0 +1,14 @@
+'''
+Hereby the variables used in formulae are explained:
+
+Tc -- temperature of the noise introduced by the cable
+T0 -- clarify later; given OR reference temperature IF reference, T0 = 290 K
+L  -- loss factor, property of a cable
+Tn -- temperature of the noise introduced by the amplifier
+F  -- noise figure, ratio of noise out to noise into the amp
+
+There are different typical values of antenna noise, L, F, etc. for different frequencies
+for downlink/uplink => we need to have a table with different options to get the right values
+
+'''
+
